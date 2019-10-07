50d7b49c3618eefb6eddf873f12592d0d6a15e31
sagarsathe2020 <sagar.sathe2020@gmail.com>
2019-10-07 19:00:53 +0530

    Added notification and logout menu for admin user

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../router.animations';
+import { AuthenticationService } from '../shared/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
@@ -9,8 +11,13 @@ import { routerTransition } from '../router.animations';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() {}
+  constructor(private authenticationService: AuthenticationService, public router: Router) {}
 
   ngOnInit() {
   }
+
+  onLoggedout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
 }
